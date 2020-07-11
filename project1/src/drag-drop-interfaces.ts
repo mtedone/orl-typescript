fa1b3fe82f7460cd765fe091a55d1ac0c7023b0b
Marco Tedone <marco.tedone@gmail.com>
2020-07-11 17:43:22 +0100

    Refactored some code with namespaces

---
+++ project1/src/drag-drop-interfaces.ts
@@ -0,0 +1,12 @@
+namespace App {
+  export interface Draggable {
+    dragStartHandler(event: DragEvent): void;
+    dragEndHanlder(event: DragEvent): void;
+  }
+
+  export interface DragTarget {
+    dragOverHandler(event: DragEvent): void;
+    dropHandler(event: DragEvent): void;
+    dragLeaveHandler(event: DragEvent): void;
+  }
+}

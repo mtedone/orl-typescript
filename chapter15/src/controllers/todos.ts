ef97e6fa439b2e2603f39371510b1cb5bb544d12
Marco Tedone <marco.tedone@gmail.com>
2020-07-12 18:08:04 +0100

    Create Node / Express with Typescript

--- chapter15/src/controllers/todos.ts
+++ chapter15/src/controllers/todos.ts
@@ -1,12 +1,40 @@
 import { RequestHandler } from 'express';
+import { v4 as uuid } from 'uuid';
 import { Todo } from '../models/todo';
 
 const TODOS: Todo[] = [];
 
 export const createTodo: RequestHandler = (req, res, next) => {
   const text = (req.body as { text: string }).text;
-  const newTodo = new Todo(Math.random.toString(), text);
+  const newTodo = new Todo(uuid(), text);
 
   TODOS.push(newTodo);
   res.status(201).json({ message: 'Created the todo', createdTodo: newTodo });
+};
+
+export const getTodos: RequestHandler = (req, res, next) => {
+  res.json({ todos: TODOS });
+};
+
+export const updateTodo: RequestHandler<{ id: string }> = (req, res, next) => {
+  const todoId = req.params.id;
+  const updatedText = (req.body as { text: string }).text;
+  const todoIndex = TODOS.findIndex((todo) => todo.id === todoId);
+  if (todoIndex < 0) {
+    throw new Error('Could not find TODO');
+  }
+  TODOS[todoIndex] = new Todo(TODOS[todoIndex].id, updatedText);
+  res.json({ message: 'Updated todo', updateTodo: TODOS[todoIndex] });
+};
+
+export const deleteTodo: RequestHandler<{ id: string }> = (req, res, next) => {
+  const todoId = req.params.id;
+  const todoIndex = TODOS.findIndex((todo) => {
+    return todo.id === todoId;
+  });
+  if (todoIndex < 0) {
+    throw new Error(`Todo with id: ${todoId} not found`);
+  }
+  const todoRemoved = TODOS.splice(todoIndex, 1);
+  res.json({ message: 'Todo removed', deletedTodo: todoRemoved });
 };
